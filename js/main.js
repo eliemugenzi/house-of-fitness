728366411fe0ae95500813632f2f800e3eef3755
eliemugenzi <eliemugenzi@gmail.com>
2018-05-03 22:59:05 +0200

    Bug fixing

--- js/main.js
+++ js/main.js
@@ -9,4 +9,41 @@ $(document).ready(()=>{
       visibility:'hidden'
     }).removeClass("slide")
   })
+
+  $(".home-link").click(()=>{
+    const path=$("#showcase").offset().top;
+    $("body,html").animate({
+      scrollTop:path
+    })
+    $("#nav-mobile").css({
+      visibility:'hidden'
+    }).removeClass("slide")
+  })
+  $(".about-link").click(()=>{
+    const path=$("#section-1").offset().top;
+    $("body,html").animate({
+      scrollTop:path
+    })
+    $("#nav-mobile").css({
+      visibility:'hidden'
+    }).removeClass("slide")
+  })
+  $(".coaches-link").click(()=>{
+    const path=$("#coaches").offset().top;
+    $("body,html").animate({
+      scrollTop:path
+    })
+    $("#nav-mobile").css({
+      visibility:'hidden'
+    }).removeClass("slide")
+  })
+  $(".contact-link").click(()=>{
+    const path=$("footer").offset().top;
+    $("body,html").animate({
+      scrollTop:path
+    })
+    $("#nav-mobile").css({
+      visibility:'hidden'
+    }).removeClass("slide")
+  })
 })
